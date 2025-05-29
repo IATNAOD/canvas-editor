20dc78309ae41ca051197bb78c7705af7dcdbe2f
IATNAOD <iatnaod@iatnaod.space>
2025-05-29 04:18:01 +0300

    fix: default button export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DefaultButton from './components/Buttons/DefaultButton';
+
 export { default as DefaultButton } from './components/Buttons/DefaultButton';
 
 export default { DefaultButton };
